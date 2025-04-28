c4d8cc811d2f8e91016a95e3e075e777054886f4
yasirufonseka <150530375+yasirufonseka@users.noreply.github.com>
2025-04-28 16:49:49 +0530

    add routing
    add routing between components and a simple message to home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch:  'full'
+    },
+    {
+        path:'home',
+        component: HomeComponent
+    },
+    {
+        path:'about',
+        component:AboutComponent
+    },
+    {
+        path:'projects',
+        component: ProjectsComponent
+    }
+    
+];
